1debc31dab43d5d83f858831e1310f3ef25e0295
camardanic <geom.camarda@gmail.com>
2020-12-15 20:56:40 +0100

    Modo in cui vengono eseguiti i pagamenti in ogni distretto

--- taxi_zone.py
+++ taxi_zone.py
@@ -23,14 +23,30 @@ args = parser.parse_args()
 A1_data_df = pd.read_csv((args.input_data))
 
 #Database di Aprile (20 righe)
-A2_data_df = A1_data_df.head(100)
+# A2_data_df = A1_data_df.head(100)
 
 #Database delle borough
 B_data_df = pd.read_csv((args.input_data1))
 
 #Ampliato il database iniziale, aggiungendo informazioni attraverso il Location ID in merito al Distretto di inizio corsa(PULocation)
-C_data_df = A2_data_df.join(B_data_df.set_index('LocationID'), on='PULocationID')
+C_data_df = A1_data_df.join(B_data_df.set_index('LocationID'), on='PULocationID')
 
 #Rimozione delle colonne superflue dal C_data_df
 C_data_df.drop(['Zone', 'service_zone'], axis=1, inplace=True)
+
+#Numero di pagamenti per ogni tipologia
+num_type_payment=C_data_df['payment_type'].value_counts()
+
+#Tipi di pagamento
+type_payment=C_data_df['payment_type'].unique()
+
+#Numero di Borough
+Borough=C_data_df['Borough'].unique()
+
+#Modo in cui vengono eseguiti i pagamenti in ogni distretto 
+df_out= pd.DataFrame(0,index=Borough, columns=type_payment)
+for i in range(len(C_data_df)): 
+    df_out.loc[(C_data_df.loc[i,'Borough'],C_data_df.loc[i,'payment_type'])] += 1 
+
+
 
